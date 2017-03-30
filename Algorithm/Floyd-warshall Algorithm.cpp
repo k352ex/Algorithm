3f47c3d8f2ab801eee7847f97fa85b5af50d7257
k352ex <k352ex@naver.com>
2017-03-30 12:23:56 +0000

    플로이드 와샬 알고리즘 정리 및 README 수정

---
+++ Algorithm/Floyd-warshall Algorithm.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#define INF 123456789   
+using namespace std;
+
+int main() {
+    
+    int n, m, distance[100][100];
+    cin >> n >> m;
+    
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            distance[i][j] = i == j ? 0 : INF;
+        }
+    }
+    
+    for(int i=0; i<m; ++i) {
+        int x, y, z;
+        cin >> x, y, z;
+        distance[x-1][y-1] = min(distance[x-1][y-1], z);
+    }
+    
+    // 경유지 -> 시작점 -> 도착점
+    for(int k=0; k<n; ++k) {
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<n; ++j) {
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+            }
+        }
+    }
+    
+    // distance[i][j]는 i에서 j로 가는 최단거리 
+    for(int i=0; i<n; ++i) {
+        for(int j=0; j<n; ++j) {
+            cout << distance[i][j] << " ";
+        }
+        cout << '\n';
+    }
+}
